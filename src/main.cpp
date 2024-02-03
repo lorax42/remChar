34e705e42b5d8c68fb76d33584c2ebd5913a5f86
lorax <142108266+lorax42@users.noreply.github.com>
2024-02-03 13:44:33 +0100

    add file read support

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 int main(int argc,char *argv[]){
-	char remove=*argv[1];
-	std::string str=argv[2];
-	
-	for(int i=str.length()-1;i>=0;i--){
-		if(str.at(i)==remove){
-			str.erase(i,1);
-		}
+	if(argc<2){
+		std::cout<<"ERROR: not enough arguments\nreturn error code 1\n";
+		return 1;
 	}
-	std::cout<<str<<std::endl;
+
+	else{
+		char remove=*argv[1];
+		std::string text;
+
+		if(std::string(argv[2])=="-f"){ // FIX: RETURNS FALSE EVEN IF -f IS GIVEN!!!
+			std::ifstream reader;
+			reader.open(argv[3]);
+
+			if(!reader){
+				std::cout<<"ERROR: couldn't open file\nreturn error code 1\n";
+				return 1;
+			}
+
+			std::string line;
+
+			while(getline(reader,line)){
+				text.append(line);
+				text.append("\n");
+			}
+
+			reader.close();
+		}
+
+		else{
+			text=argv[2];
+		}
+
+		for(int i=text.length()-1;i>=0;i--){
+			if(text.at(i)==remove){
+				text.erase(i,1);
+			}
+		}
+
+		std::cout<<text<<std::endl;
+	}
 
 	return 0;
 }
